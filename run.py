e041b92bf5e6af2937b5054c6ebc1de132692731
Nahuel Cosme <ncdv.2003@gmail.com>
2023-11-26 20:30:38 +0000

    Print output modified

--- run.py
+++ run.py
@@ -8,13 +8,15 @@ from timeit import default_timer as timer
 # [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
 # [<Node B>, <Node F>, <Node S>, <Node A>] : 211 + 99 + 140 = 450
 
-def show_result(result, execution_time):
-    print("Path: " + str(result.node.path()))
-    print("Generated nodes: " + str(result.generados))
-    print("Visited nodes: " + str(result.visitados))
-    print("Total cost: " + str(result.node.path_cost))
-    print("Execution time: " + str(execution_time) + " ms")
-    print("=" * 40)
+def show_result(method, result, execution_time):
+        print("Path: " + str(result.node.path()))
+        print("Generated nodes\t" + 
+              "Visited nodes\tPath cost\tRuntime")
+        print(str(result.generados) +
+              "\t" * 2 + str(result.visitados) +
+              "\t" * 2 + str(result.node.path_cost) +
+              "\t" * 2 + str(execution_time) + " ms")
+        print("=" * 40)
 
 def do_search(problem, method):    
     print ("Search method: " + method.__name__)
@@ -22,7 +24,7 @@ def do_search(problem, method):
     result = method(problem)
     end = timer()
     execution_time = round((end - start) * 1000, 3)
-    show_result(result, execution_time)
+    show_result(method, result, execution_time)
 
 def make_tests():
     tests = [
